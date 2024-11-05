5e877b6579eb2ba6c67258b0f1df18a1f372b41e
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2024-11-05 17:48:32 +0100

    Advances in the JS file for the inputs

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
 /* Declare variables to use */
 
 let button = document.querySelector(".calculate-repayments"),
-  repaymentContainer = document.querySelector(".repayment-container"),
-  interestContainer = document.querySelector(".interest-container");
+  interestContainer = document.querySelector(".interest-container"),
+  inputsCheckbox = document.querySelector(".inputs-checkbox");
 
-button.addEventListener("click", () => {
-  repaymentContainer.style.backgroundColor = "red";
-});
+button.addEventListener("click", () => {});
+
+/* Function for REPAYMENTS input */
+
+let checkRepayments = () => {
+  let repaymentInput = document.querySelector(".input_repayment"),
+    repaymentContainer = document.querySelector(".repayment-container"),
+    repaymentCheckbox = document.querySelector(".checkbox_repayment");
+
+  repaymentInput.addEventListener("click", () => {
+    if (repaymentInput.checked) {
+      repaymentContainer.style.border = "2px solid var(--Lime)";
+    } else {
+      repaymentContainer.style.border = "var(--border-inputs)";
+    }
+  });
+};
+
+/* Function for INTEREST input */
+
+let checkInterest = () => {
+  let interestInput = document.querySelector(".input_interest"),
+    interestContainer = document.querySelector(".interest-container"),
+    interestCheckbox = document.querySelector(".checkbox_interest");
+
+  interestInput.addEventListener("click", () => {
+    if (interestInput.checked) {
+      interestContainer.style.border = "2px solid var(--Lime)";
+    } else {
+      interestContainer.style.border = "var(--border-inputs)";
+    }
+  });
+};
+
+checkRepayments();
+checkInterest();
